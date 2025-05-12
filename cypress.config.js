ce1f68a9608bba21ab3f097ba76e60291b42c5f2
kenneth <kennethpz1702@gmail.com>
2025-05-12 08:54:15 -0600

    configuracion de reporte

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer'); //Enlace de Cypress allure report
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+    allureWriter(on, config);
+      return config; //También se consigue en cypress allure report
     },
   },
 });
